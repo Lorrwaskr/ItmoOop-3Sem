427a8c6665a1f92ef202ba37d834fcd16fec1060
Lorrwaskr <tsydenov.alg@gmail.com>
2022-01-08 20:12:13 +0800

    add milliseconds

--- Backups/RestorePoint/FileRestorePoint.cs
+++ Backups/RestorePoint/FileRestorePoint.cs
@@ -14,7 +14,7 @@ namespace Backups.RestorePoint
             AlgorithmType = algorithmType;
             JobObjects = jobObjects.ToList();
             CreationTime = DateTime.UtcNow;
-            Name = $"{name}_{CreationTime.Day}.{CreationTime.Month}.{CreationTime.Year}_{CreationTime.Hour}.{CreationTime.Minute}.{CreationTime.Second}";
+            Name = $"{name}_{CreationTime.Day}.{CreationTime.Month}.{CreationTime.Year}_{CreationTime.Hour}.{CreationTime.Minute}.{CreationTime.Second}.{CreationTime.Millisecond}";
         }
 
         public string Name { get; set; }
